6a890644bd9822042fcf8d5f4a28a37182de3424
K-Kizuku <kotani.kizuku@gmail.com>
2023-03-12 17:36:40 +0900

    todays commit

---
+++ allo_hackathon.Server/Api/Controller.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+//using BlazorWasm.Grains;
+//using BlazorWasm.Models;
+using System.ComponentModel.DataAnnotations;
+using Orleans;
+
+namespace Server.Silo.Api;
+
+[ApiController]
+[Route("api/todo")]
+public class TodoController : ControllerBase
+{
+    private readonly IGrainFactory _factory;
+
+    public TodoController(IGrainFactory factory) => _factory = factory;
+
+    [HttpGet("/")]
+    public ActionResult GetAsync() => Ok();
+}
