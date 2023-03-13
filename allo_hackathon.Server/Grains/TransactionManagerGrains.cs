e94ae30e14250c7862a9835f71859d98fbc91382
K-Kizuku <kotani.kizuku@gmail.com>
2023-03-13 21:31:35 +0900

    transactionのエンドポイントの実装

---
+++ allo_hackathon.Server/Grains/TransactionManagerGrains.cs
@@ -0,0 +1,37 @@
+﻿using Orleans.Runtime;
+using System.Collections.Immutable;
+
+namespace Server.Grains;
+
+public class TransactionManagerGrain : Grain, ITransactionManagerGrain
+{
+    private readonly IPersistentState<TransactionsState> _state;
+
+    private string GrainKey = "Transactions";
+
+    public TransactionManagerGrain(
+        [PersistentState("TransactionsState")] IPersistentState<TransactionsState> state) => _state = state;
+
+    public async Task RegisterAsync(Guid transactionKey)
+    {
+        _state.State.Transactions.Add(transactionKey);
+        await _state.WriteStateAsync();
+    }
+
+    public async Task UnregisterAsync(Guid transactionKey)
+    {
+        _state.State.Transactions.Remove(transactionKey);
+        await _state.WriteStateAsync();
+    }
+
+    public Task<List<Guid>> GetAllAsync() =>
+        Task.FromResult(new List<Guid>(_state.State.Transactions));
+
+    [GenerateSerializer]
+    public class TransactionsState
+    {
+        [Id(0)]
+        public HashSet<Guid> Transactions { get; set; } = new();
+    }
+}
+
---
+++ allo_hackathon.Server/Models/Transaction.cs
@@ -0,0 +1,18 @@
+﻿namespace Server.Models;
+
+
+[Immutable]
+[GenerateSerializer]
+public record class Transaction(
+    // トランザクションID
+    Guid Key,
+    Guid TokenId,
+    DateTime? CreatedAt,
+    // 誰から送られたか
+    string SendFrom,
+    // 誰に送られたか
+    string SendTo,
+    // いくら送金したか
+    double Cost,
+    bool IsFarst);
+
