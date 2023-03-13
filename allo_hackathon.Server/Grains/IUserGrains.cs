351b00460ae3031f25a9ea1f3f2469ae63b93562
K-Kizuku <kotani.kizuku@gmail.com>
2023-03-13 12:35:51 +0900

    merge

---
+++ allo_hackathon.Server/Grains/IUserGrains.cs
@@ -0,0 +1,13 @@
+﻿using Server.Models;
+
+namespace Server.Grains;
+
+public interface IUserGrains : IGrainWithGuidKey
+{
+    Task SetAsync(Users users);
+
+    //Task ClearAsync();
+
+    Task<Users?> GetAsync();
+}
+
---
+++ allo_hackathon.Server/Models/Users.cs
@@ -0,0 +1,20 @@
+﻿namespace Server.Models;
+
+[Immutable]
+[GenerateSerializer]
+public record class Users(
+    Guid Key,
+    DateTime? CreatedAt,
+    string Name,
+    // 1日1回のトークンを受け取ったかどうか
+    bool IsReceived,
+    // 今持ってるトークンのリスト
+    TokenItem?[] TokenList,
+    // 自分が送ったトークンのリスト
+    TokenItem?[] SendTo,
+    // 自分に送られたトークンのリスト
+    TokenItem?[] SendFrom,
+    // 自分のトークンの合計数
+    double? MyToken,
+    DateTime? DeletedAt = null);
+
